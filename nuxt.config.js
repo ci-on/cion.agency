c1525995f25c0d7e68ecc83ad1a34496035a66cd
Chris Breuer <chrisbreuer93@gmail.com>
2019-04-11 10:37:00 -0700

    adjust favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,9 +32,6 @@ module.exports = {
       { rel: 'apple-touch-icon', sizes:"152x152", href: '/favicon/apple-icon-152x152.png' },
       { rel: 'apple-touch-icon', sizes:"180x180", href: '/favicon/apple-icon-180x180.png' },
       { rel: 'icon', type:"image/png", sizes:'192x192', href: '/favicon/android-icon-192x192.png' },
-      { rel: 'icon', type:"image/png", sizes:'32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type:"image/png", sizes:'96x96', href: '/favicon-96x96.png' },
-      { rel: 'icon', type:"image/png", sizes:'16x16', href: '/favicon-16x16.png' }
     ]
   },
   /*
