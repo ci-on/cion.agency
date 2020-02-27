9709fb762cd23594c4949c4da78b1eeaea484e8a
Chris Breuer <chrisbreuer93@gmail.com>
2020-02-27 15:25:11 -0800

    revamp

---
+++ components/ChrisQuote.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="py-12 bg-white overflow-hidden md:py-20 lg:py-24">
+    <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="relative">
+        <cion-logo class="mx-auto h-10" />
+
+        <blockquote class="mt-8">
+          <div
+            class="max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900"
+          >
+            <p>
+              &ldquo;CION is a Software Development & Creative Agency based in
+              Southern California. Time and time again we are able to help our
+              Clients grow what matters most to them, relationships. &rdquo;
+            </p>
+          </div>
+          <footer class="mt-8">
+            <div class="md:flex md:items-center md:justify-center">
+              <div class="md:flex-shrink-0">
+                <img
+                  class="mx-auto h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </div>
+              <div
+                class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center"
+              >
+                <div class="text-base leading-6 font-medium text-gray-900">
+                  Chris Breuer
+                </div>
+
+                <svg
+                  class="hidden md:block mx-1 h-5 w-5 text-indigo-600"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M11 0h3L9 20H6l5-20z" />
+                </svg>
+
+                <div class="text-base leading-6 font-medium text-gray-500">
+                  Founder & Tech Lead, CION Agency
+                </div>
+              </div>
+            </div>
+          </footer>
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CionLogo from './CionLogo'
+
+export default {
+  components: {
+    CionLogo
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,102 +1,55 @@
-const pkg = require('./package')
-
-module.exports = {
-  mode: 'spa',
-
+export default {
+  mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'CION Agency',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'CION is a software development agency specializing in modern PHP, Laravel, Craft CMS, and Vue.js development' },
-      { name: 'theme-color', content: '#00bcd4'},
-      { name: 'keywords', content: 'CION,Website,Agency,Artificial Intelligence'},
-      { name: 'description', content: 'CION is a software development agency specializing in modern PHP, Laravel, Craft CMS, and Vue.js development'},
-      { property: 'og:description', content: 'CION is a software development agency specializing in modern PHP, Laravel, Craft CMS, and Vue.js development'},
-      { property: 'og:title', content: 'CION Agency | Product Development for Web & Mobile | Laravel + Vue.js | Artificial Intelligence'},
-      { property: 'og:image' , content: 'https://cion.agency/favicon/cion-logo.png' },
-      { property: 'og:image:alt' , content: 'CION Logo | Consulting Agency' },
-      { property: 'og:url' , content: 'https://cion.agency/' },
-      { property: 'og:type' , content: 'website' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon/cion-logo.png' },
-      { rel: 'apple-touch-icon', sizes:"57x57", href: '/favicon/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes:"60x60", href: '/favicon/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes:"72x72", href: '/favicon/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes:"76x76", href: '/favicon/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes:"114x114", href: '/favicon/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes:"120x120", href: '/favicon/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes:"144x144", href: '/favicon/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes:"152x152", href: '/favicon/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes:"180x180", href: '/favicon/apple-icon-180x180.png' },
-      // { rel: 'icon', type:"image/png", sizes:'192x192', href: '/favicon/android-icon-192x192.png' },
-      // { rel: 'icon', type:"image/png", sizes:'32x32', href: '/favicon-32x32.png' },
-      // { rel: 'icon', type:"image/png", sizes:'96x96', href: '/favicon-96x96.png' },
-      // { rel: 'icon', type:"image/png", sizes:'16x16', href: '/favicon-16x16.png' }
-    ],
-    script: [
       {
-        hid: 'fathom-analytics',
-        innerHTML: `
-(function(f, a, t, h, o, m){
-a[h]=a[h]||function(){
-(a[h].q=a[h].q||[]).push(arguments)
-};
-o=f.createElement('script'),
-m=f.getElementsByTagName('script')[0];
-o.async=1; o.src=t; o.id='fathom-script';
-m.parentNode.insertBefore(o,m)
-})(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-fathom('set', 'siteId', 'JXUSBEEK');
-fathom('trackPageview');
-        `,
-        type: 'text/javascript',
-        charset: 'utf-8'
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
       }
-    ]
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
-
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/css/tailwind.css'
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
   ],
-
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/vue-smooth-scroll'
-  ],
-
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-138220738-1',
-      dev: false
-    }]
-  ],
-
-  /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
