708034e736e4f9261fe28d6c274678af6a38bd2a
Chris Breuer <chrisbreuer93@gmail.com>
2020-02-29 23:04:11 -0800

    wip

--- components/LogoCloud.vue
+++ components/LogoCloud.vue
@@ -7,37 +7,112 @@
         Trusted by small and large companies
       </p>
       <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
-          <img class="max-h-12" src="~/assets/aetna-logo.svg" alt="Aetna" />
-        </div>
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
-          <img class="max-h-12" src="~/assets/meema-logo.svg" alt="Meema" />
-        </div>
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'aetna'"
+          @mouseleave="hovering = null"
+        >
           <img
             class="max-h-12"
-            src="~/assets/girlgaze-logo.svg"
+            :src="
+              hovering === 'aetna'
+                ? '/aetna-logo-color.svg'
+                : '/aetna-logo-gray.svg'
+            "
+            alt="Aetna"
+          />
+        </div>
+
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'meema'"
+          @mouseleave="hovering = null"
+        >
+          <img
+            class="max-h-12"
+            :src="
+              hovering === 'meema'
+                ? '/meema-logo-color.svg'
+                : '/meema-logo-gray.svg'
+            "
+            alt="Meema"
+          />
+        </div>
+
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'girlgaze'"
+          @mouseleave="hovering = null"
+        >
+          <img
+            class="max-h-12"
+            :src="
+              hovering === 'girlgaze'
+                ? '/girlgaze-logo-color.svg'
+                : '/girlgaze-logo-gray.svg'
+            "
             alt="Girlgaze"
           />
         </div>
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
+
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'greenline'"
+          @mouseleave="hovering = null"
+        >
           <img
             class="max-h-12"
-            src="~/assets/greenline-logo.svg"
+            :src="
+              hovering === 'greenline'
+                ? '/greenline-logo-color.svg'
+                : '/greenline-logo-gray.svg'
+            "
             alt="Greenline"
           />
         </div>
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
+
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'carefree'"
+          @mouseleave="hovering = null"
+        >
           <img
             class="max-h-12"
-            src="~/assets/carefree-logo.svg"
+            :src="
+              hovering === 'carefree'
+                ? '/carefree-logo-color.svg'
+                : '/carefree-logo-gray.svg'
+            "
             alt="CareFree"
           />
         </div>
-        <div class="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
-          <img class="max-h-8" src="~/assets/mutesix-logo.svg" alt="Mutesix" />
+
+        <div
+          class="flex justify-center col-span-1 px-8 py-8 bg-gray-50"
+          @mouseover="hovering = 'mutesix'"
+          @mouseleave="hovering = null"
+        >
+          <img
+            class="max-h-8"
+            :src="
+              hovering === 'mutesix'
+                ? '/mutesix-logo-color.svg'
+                : '/mutesix-logo-gray.svg'
+            "
+            alt="Mutesix"
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      hovering: null
+    }
+  }
+}
+</script>
